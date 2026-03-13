59132201d1a153fed0f50493848b7b423a2188a7
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2026-03-13 13:54:21 -0300

    iniciando o projeto

---
+++ src/main/java/com/github/eliaspinheiropereira/portaldoservidorpabotdownloader/PortaldoservidorpabotdownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.github.eliaspinheiropereira.portaldoservidorpabotdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaldoservidorpabotdownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaldoservidorpabotdownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/eliaspinheiropereira/portaldoservidorpabotdownloader/PortaldoservidorpabotdownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eliaspinheiropereira.portaldoservidorpabotdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaldoservidorpabotdownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
