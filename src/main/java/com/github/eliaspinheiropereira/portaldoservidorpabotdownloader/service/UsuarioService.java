6bae2fe32eab1dba61abf4dd18ac5dc184a99a7a
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2026-03-17 19:33:58 -0300

    finalizando a chamada ao mapstruct

--- src/main/java/com/github/eliaspinheiropereira/portaldoservidorpabotdownloader/service/UsuarioService.java
+++ src/main/java/com/github/eliaspinheiropereira/portaldoservidorpabotdownloader/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.github.eliaspinheiropereira.portaldoservidorpabotdownloader.service;
 
 import com.github.eliaspinheiropereira.portaldoservidorpabotdownloader.component.*;
 import com.github.eliaspinheiropereira.portaldoservidorpabotdownloader.controller.dto.UsuarioDTO;
+import com.github.eliaspinheiropereira.portaldoservidorpabotdownloader.controller.mappers.UsuarioMapper;
 import com.github.eliaspinheiropereira.portaldoservidorpabotdownloader.model.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.WebDriver;
@@ -13,18 +14,12 @@ public class UsuarioService {
 
     private final BrowserService browserService;
     private final LoginService loginService;
-    private final CriandoPastaService criandoPastaService;
     private final ContraChequeService contraChequeService;
     private final SelecionarElementosService selecionarElementosService;
+    private final UsuarioMapper usuarioMapper;
 
     public void login(UsuarioDTO usuarioDTO) {
-        Usuario usuario = new Usuario();
-        usuario.setUrl(usuarioDTO.url());
-        usuario.setUsername(usuarioDTO.username());
-        usuario.setSenha(usuarioDTO.senha());
-        usuario.setAnoInicial(usuarioDTO.anoInicial());
-        usuario.setAnoFinal(usuarioDTO.anoFinal());
-        usuario.setContrato(usuarioDTO.contrato());
+        Usuario usuario = this.usuarioMapper.toEntity(usuarioDTO);
 
         WebDriver driver = abrindoSite(usuario.getUrl());
         fazendoLogin(usuario.getUsername(), usuario.getSenha(), driver);
